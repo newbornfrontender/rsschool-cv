30234e375e0c9397e6277eb166c9954d6f076456
newbornfrontender <newbornfrontender@gmail.com>
2020-02-12 10:40:37 +0300

    feat: create main js file

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import { TitledSection } from '/components/titled-section.js';
+import { FancyLogo } from '/components/fancy-logo.js';
+
+customElements.define('titled-section', TitledSection);
+customElements.define('fancy-logo', FancyLogo);
