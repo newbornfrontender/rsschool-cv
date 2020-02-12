c08c8a50a7c812b7ed1713cda4888c8d183352d3
newbornfrontender <newbornfrontender@gmail.com>
2020-02-12 11:37:06 +0300

    feat: add user info block

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 import { TitledSection } from '/components/titled-section.js';
 import { FancyLogo } from '/components/fancy-logo.js';
+import { UserInfo } from '/components/user-info.js';
 
 customElements.define('titled-section', TitledSection);
 customElements.define('fancy-logo', FancyLogo);
+customElements.define('user-info', UserInfo);
